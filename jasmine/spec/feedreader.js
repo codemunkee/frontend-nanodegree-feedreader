0a9f90194a9742b55b464cc2c77d3fe13fb98cac
Russ Nealis <codemunkee@gmail.com>
2017-10-04 19:55:06 -0700

    added tests to ensure feed data is displayed upon async req

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,26 +1,10 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    /* Test that our RSS Feeds are defined correctly */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -41,9 +25,9 @@ $(function() {
         });
     });
 
+    /* Test our hidden menu works as it should */
     describe('The menu', function() {
-        // just make sure we set our menu state back to
-        // its original state
+        // fixtures make sure we set our menu state back to its original state
         beforeEach(function() {
             $('body').addClass('menu-hidden');
         });
@@ -62,15 +46,23 @@ $(function() {
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(false);
         });
+    });
 
-        /* TODO: Write a new test suite named "Initial Entries" */
+    /* Test that our initial feed entries are displayed as expected */
+    describe('Initial Entries', function () {
+        beforeEach(function(done) {
+            // load the first defined feed and wait for the async request to finish
+            // (we pass the done function, provided by jasmine, as a callback to
+            // wait for the request to complete)
+            loadFeed(0, done);
+        });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        /* there is at least one feed entry rendered from our entry template */
+        it('has an .entry element within the .feed container', function () {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+        });
+
+    });
 
         /* TODO: Write a new test suite named "New Feed Selection" */
 
@@ -78,5 +70,4 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    });
 }());
